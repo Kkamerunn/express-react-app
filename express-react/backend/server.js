1c2f7fb78039d086c4eba6e5f406a3c6e009ab7e
LucasGDG <digenovalucas@gmail.com>
2020-03-03 16:07:12 -0300

    packages & database connection

---
+++ express-react/backend/server.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected!');
+});
